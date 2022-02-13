046995bdc0eb2642bdfe906312470f2a2862a6d9
SergeiVlasov1 <98306472+SergeiVlasov1@users.noreply.github.com>
2022-02-13 12:33:12 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         int eaters = 5; // сколько людей будут есть
-
         int water = 3000; // миллилитров воды
         int potatoes = 5; // картофелин
         int chicken = 6; // куриных бёдер
